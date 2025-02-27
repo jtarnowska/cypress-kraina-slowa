4cfa81c3df4e4a6451be0e2021b3cbe9cdaf4b8d
Joanna <joanna.tarnowska1@gmail.com>
2025-02-27 20:34:15 +0100

    Added eslint, prettier and husky and modified cypress default config

--- cypress/e2e/title.cy.js
+++ cypress/e2e/title.cy.js
@@ -1,8 +1,10 @@
-describe ('Test of the title of the page Kraina Slowa', () => {
-    it ('Check that title is correct', () => {
-        // Open the page
-        cy.visit('https://krainaslowa.com/');
-        // Check the title of the page
-        cy.get('title').contains('Odkryj Słowa, które Zmienią Twój Dzień | Cytaty i Sentencje | Kraina Słowa')
-    });
+describe('Test of the title of the page Kraina Slowa', () => {
+  it('Check that title is correct', () => {
+    // Open the page
+    cy.visit('https://krainaslowa.com/');
+    // Check the title of the page
+    cy.get('title').contains(
+      'Odkryj Słowa, które Zmienią Twój Dzień | Cytaty i Sentencje | Kraina Słowa',
+    );
+  });
 });
