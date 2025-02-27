d4e55843bd29bac00bcb3a5887d9e69c52535aa3
Joanna <joanna.tarnowska1@gmail.com>
2025-02-27 13:16:26 +0100

    Initial commit

---
+++ cypress/e2e/title.cy.js
@@ -0,0 +1,8 @@
+describe ('Test of the title of the page Kraina Slowa', () => {
+    it ('Check that title is correct', () => {
+        // Open the page
+        cy.visit('https://krainaslowa.com/');
+        // Check the title of the page
+        cy.get('title').contains('Odkryj Słowa, które Zmienią Twój Dzień | Cytaty i Sentencje | Kraina Słowa')
+    });
+});
